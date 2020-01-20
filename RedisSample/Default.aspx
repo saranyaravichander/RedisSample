bdc8bf3687df04b3c5aff0cb2a257fab9dfe3f82
saranyaravichander <saranyaravichander@live.com>
2020-01-20 10:33:32 -0500

    Add project files.

---
+++ RedisSample/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RedisSample._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+       
+    </div>
+    <div>
+        Enter the key you want to retreive <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><t\></t> [Enter only keys : key1, key2, key3, key4"]
+        <br /><asp:Button ID="Button1" runat="server" Text="Get" OnClick="Button1_Click" />
+    </div>
+    <br />
+    <div>
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+
+</asp:Content>
---
+++ RedisSample/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using StackExchange.Redis;
+using System.Configuration;
+
+namespace RedisSample
+{
+    public partial class _Default : Page
+    {
+        static Dictionary<string, string> database = new Dictionary<string, string>
+        {
+            { "key1","answer1" },
+            { "key2","answer2" },
+            { "key3","answer3" },
+            { "key4","answer4" },
+        };
+
+        IDatabase cache = lazyConnection.Value.GetDatabase();
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var cachedValue = cache.StringGet(TextBox1.Text);
+
+            if (database.ContainsKey(TextBox1.Text))
+            {
+
+               if (cachedValue == RedisValue.EmptyString || cachedValue == RedisValue.Null)
+                {
+                    Label1.Text = "Result : " + database[TextBox1.Text] + " || Got from the database";
+                    cache.StringSet(TextBox1.Text, database[TextBox1.Text]);
+                }
+                else
+                {
+                    Label1.Text = "Result : " + cache.StringGet(TextBox1.Text).ToString() + " || Got from the cache";
+                }
+            }
+            else
+            {
+                Label1.Text = "Enter only keys : key1, key2, key3, key4";
+            }
+        }
+    }
+}
